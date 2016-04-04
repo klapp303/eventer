ec4b7c1df0694072accb99209c4246938a97295a
klapp303 <klapp303@gmail.com>
2016-04-05 03:33:40 +0900

    TOPページの直近のイベント一覧の範囲をオプション値で指定する

--- app/Controller/TopController.php
+++ app/Controller/TopController.php
@@ -4,7 +4,7 @@ App::uses('AppController', 'Controller');
 
 class TopController extends AppController {
 
-	public $uses = array('Event', 'EventsDetail', 'EventsEntry'/*, 'EventUser'*/); //使用するModel
+	public $uses = array('Event', 'EventsDetail', 'EventsEntry'/*, 'EventUser'*/, 'Option'); //使用するModel
 
   public function beforeFilter() {
       parent::beforeFilter();
@@ -38,7 +38,11 @@ class TopController extends AppController {
       $this->set('event_today_lists', $event_today_lists);
   
       //直近の予定
-      $event_current_lists = $this->EventsEntry->searchEntryDate($this->Auth->user('id'), date('Y-m-d', strtotime('1 day')), date('Y-m-d', strtotime('1 month')));
+      $CURRENT_EVENT_OPTION = $this->Option->find('first', array( //オプション値を取得
+          'conditions' => array('Option.title' => 'CURRENT_EVENT_KEY')
+      ));
+      $CURRENT_EVENT_KEY = $CURRENT_EVENT_OPTION['Option']['key'];
+      $event_current_lists = $this->EventsEntry->searchEntryDate($this->Auth->user('id'), date('Y-m-d', strtotime('1 day')), date('Y-m-d', strtotime($CURRENT_EVENT_KEY.' day')));
       foreach ($event_current_lists AS $key => $event) {
         if ($event['EventsEntry']['status'] == 3 || $event['EventsEntry']['status'] == 4) { //落選 or 見送りならば表示しない
           unset($event_current_lists[$key]);
