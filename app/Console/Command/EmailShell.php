8c5d6af44eb6cd54d2c005af61d2e8088c8a95a0
klapp303 <klapp303@gmail.com>
2016-04-18 19:34:43 +0900

    メール機能をshellに記述

---
+++ app/Console/Command/EmailShell.php
@@ -0,0 +1,64 @@
+<?php
+
+class EmailShell extends Shell {
+  public $uses = array('User', 'EventsEntry'); //使用するModel
+  
+  public function startup() {
+      parent::startup();
+  }
+
+  public function main() {
+      $this->out('メール送信処理を開始します');
+      
+      $user_lists = $this->User->find('all', array(
+          'conditions' => array('User.id !=' => 1)
+      ));
+      
+      foreach ($user_lists AS $user) {
+        //3日分の予定を取得
+        $event_lists = $this->EventsEntry->searchEntryDate($user['User']['id'], date('Y-m-d'), date('Y-m-d', strtotime('2 day')));
+        foreach ($event_lists AS $key => &$event) {
+          if ($event['EventsEntry']['status'] == 3 || $event['EventsEntry']['status'] == 4) { //落選 or 見送りならば除外する
+            unset($event_lists[$key]);
+          }
+          $event['EventsEntry']['date_status'] = null;
+          $entryDateColumn = $this->EventsEntry->getDateColumn();
+          foreach ($entryDateColumn AS $key => $column) {
+            if ($event['EventsEntry'][$column] >= date('Y-m-d 00:00:00') && $event['EventsEntry'][$column] <= date('Y-m-d 23:59:59', strtotime('2 day'))) {
+              $event['EventsEntry']['date_status'] = $key;
+            }
+          }
+          if ($event['EventsEntry']['date_event'] >= date('Y-m-d 00:00:00') && $event['EventsEntry']['date_event'] <= date('Y-m-d 23:59:59', strtotime('2 day'))) {
+            $event['EventsEntry']['date_status'] = '近日開催';
+          }
+          if (date('Y-m-d', strtotime($event['EventsEntry']['date_event'])) == date('Y-m-d')) {
+            $event['EventsEntry']['date_status'] = '本日開演';
+          }
+        }
+        unset($event);
+        
+        //イベントの予定があればメールを送信
+        if ($event_lists) {
+          //$user = $this->User->find('first', array('conditions' => array('User.id' => $id)));
+          $email = new CakeEmail('gmail');
+          $email->to($user['User']['username'])
+                ->subject('【イベ幸】イベント予定のお知らせ')
+                ->template('event_schedule', 'eventer_mail')
+                ->viewVars(array(
+                    'name' => ($user['User']['handlename'])? $user['User']['handlename']: $user['User']['username'],
+                    'event_lists' => $event_lists,
+                    'entryDateColumn' => $entryDateColumn
+                )); //mailに渡す変数
+          if ($email->send()) {
+            //$this->out('メールに予定を送信しました');
+          } else {
+            $this->out('メールを送信できませんでした');
+          }
+        } else {
+          $this->out('現在は予定がありません');
+        }
+      }
+      
+      $this->out('メール送信処理は成功しました');
+  }
+}
